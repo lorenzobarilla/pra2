decf8eadd5a9f9e06af7e1b8c2dc6e4084809a1a
lorenzobarilla <37076142+lorenzobarilla@users.noreply.github.com>
2021-06-19 16:26:33 +0200

    fence1

--- sketch2.js
+++ sketch2.js
@@ -19,6 +19,14 @@ var myPosY;
 var myCanvas;
 
 
+var fence1;
+var fence1circle;
+var fence1circleX;
+var fence1circleY;
+let fence1LatPos;
+let fence1LongPos;
+
+
 
 
 function preload() {
@@ -34,14 +42,36 @@ function setup() {
 
 
   //Get device position every 10ms and execute callback showPosition
-  intervalCurrentPosition(showPosition, 100);
+  intervalCurrentPosition(showPosition, 50);
 
-  myCircle = new Ball(myCircleX, myCircleY);
+  myCircle = new Ball('white', 25);
+
+  //FENCES
+  const fenceRadius = 0.006; //in km
+
+  fenceOptions = {
+    enableHighAccuracy: true,
+  };
+
+  const fence1LatIncr = fencePosIncr;
+  const fence1LongIncr = -fencePosIncr;
+  fence1LatPos = myInitLoc.latitude + fence1LatIncr;
+  fence1LongPos = myInitLoc.longitude + fence1LongIncr;
+  fence1 = new geoFenceCircle(fence1LatPos, fence1LongPos, fenceRadius, insideTheFence1, outsideTheFence, 'km', fenceOptions);
+
+  fence1circle = new Ball('yellow', 50);
+  fence1circleX = map(fence1LongPos, myInitLoc.longitude - fencePosIncr, myInitLoc.longitude + fencePosIncr, 0, myCanvas.width);
+  fence1circleY = map(fence1LatPos, myInitLoc.latitude - fencePosIncr, myInitLoc.latitude + fencePosIncr, 0, myCanvas.height);
+
+
 
 
 
 }
 
+
+function insideTheFence1() {}
+function outsideTheFence() {}
 
 function draw() {
   clear();
@@ -51,6 +81,8 @@ function draw() {
 
 
   myCircle.display(myCircleX, myCircleY);
+
+  fence1circle.display(fence1circleX, fence1circleY);
 
 }
 
@@ -70,23 +102,16 @@ function showPosition(position) {
 }
 
 
-function Ball() {
+function Ball(_fill, _size) {
   // Properties defined by constructor
+  this.fill = _fill;
+  this.size = _size;
 
-  // Hardcoded properties
-  this.size = 50;
-
-
-  // Methods
-  // this.move = function() {
- 	// this.x += this.speed;
- 	// this.y += this.speed;
-  // }
 
   this.display = function(_x, _y) {
     this.x = _x;
     this.y = _y;
-	fill("white");
-	ellipse(this.x, this.y, this.size);
+	  fill(this.fill);
+	  ellipse(this.x, this.y, this.size);
   }
 }
